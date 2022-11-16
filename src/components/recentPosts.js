534bbab232384fc699537a06a05dbb324eb915a0
Yongkang Lu <yongkanglu110@gmail.com>
2022-11-16 02:41:29 -0600

    added mapStateToProps(state){} in the recentPosts.js component and rendered the                                state.posts.recentPosts with li tags

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+
 import { connect } from 'react-redux';
-import SearchBar from './searchBar';
+
 import * as actions from '../actions';
 
 class RecentPosts extends Component {
@@ -9,16 +10,26 @@ class RecentPosts extends Component {
         this.props.fetchRecentPosts();
     }
 
+    renderPosts = function() {
+        const posts = this.props.recentPosts.map((post, index) => {
+            if(index < 3) {
+                return (
+                    <li key={index}>
+                        {post.title}
+                    </li>
+                )   
+            }
+        })
+        return posts
+    }
+
     render() {
         return (
-            <div className='recent-posts'>
-                <div className='recent-posts__wrapper'>
-                    <div className='recent-posts__heading'>Recent Posts</div>
-                    <ul className='recent-posts__posts'>
-                        <li>recent post 0</li>
-                        <li>recent post 1</li>
-                        <li>recent post 2</li>
-                        <li>recent post 3</li>
+            <div className="recent-posts">
+                <div className="recent-posts__wrapper">
+                    <div className="recent-posts__heading">Recent Posts</div>
+                    <ul className="recent-posts__posts">
+                        {this.renderPosts()}
                     </ul>
                 </div>
             </div>
@@ -26,4 +37,10 @@ class RecentPosts extends Component {
     }
 }
 
-export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state) {
+    return {
+        recentPosts: state.posts.recentPosts
+    }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
